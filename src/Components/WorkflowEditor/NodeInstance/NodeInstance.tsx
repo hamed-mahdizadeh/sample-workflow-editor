4fbb44f0946241b8c6c065a21a1a8b83a294f104
Hamed Mahdizadeh <hamedmahdizadeh@gmail.com>
2023-08-13 09:18:27 +0200

    Add move logic for workflow nodes

--- src/Components/WorkflowEditor/NodeInstance/NodeInstance.tsx
+++ src/Components/WorkflowEditor/NodeInstance/NodeInstance.tsx
@@ -1,10 +1,30 @@
-import { useEffect, useRef } from "react";
+import React from "react";
+import { useEffect, useRef, useState } from "react";
 import { NodeInstanceData } from "../../../types/workflow-types";
 import classes from "./NodeInstance.module.css";
 
 const NodeInstance = ( {node }: {node: NodeInstanceData}) => {
     const nodeRef = useRef<SVGSVGElement | null>(null);
     const nodeTextRef = useRef<SVGTextElement>(null);
+    const [isSelected, setIsSelected] = useState(false);
+
+    useEffect(() => {
+        const mouseMoveHandler = (event: globalThis.MouseEvent) => {
+            const element = nodeRef.current;
+            if(!element) {
+                return;
+            }
+            element.style.top = `${event.screenY - node.height * 1.5}px`;
+            element.style.left = `${event.screenX - node.width / 2}px`;
+        };
+        if(isSelected) {
+            window.addEventListener('mousemove', mouseMoveHandler)
+        }
+        return () => {
+            window.removeEventListener('mousemove', mouseMoveHandler);
+        }
+
+    }, [isSelected]);
 
     useEffect(() => {
         if(nodeRef.current && node.coordinates) {
@@ -15,17 +35,25 @@ const NodeInstance = ( {node }: {node: NodeInstanceData}) => {
     }, [node, nodeRef])
 
     const handleMouseLeave = () => {
-
+        setIsSelected(false);
     }
 
     const handleMouseUp = () => {
-
+        setIsSelected(false);
     }
 
+    const mouseDownHandler = () => {
+        setIsSelected(true);
+    }
+
+    const nodeClasses = isSelected ? `${classes.nodeInstance} ${classes.moving}` : `${classes.nodeInstance}`;
+
     return (
-            <svg ref={nodeRef} className={classes.nodeInstance} width={node.width} height={node.height} onMouseOut={handleMouseLeave} onMouseUp={handleMouseUp}>
+            <svg ref={nodeRef} className={nodeClasses} width={node.width} height={node.height} onMouseDown={mouseDownHandler} onMouseOut={handleMouseLeave} onMouseUp={handleMouseUp}>
                 <rect height={node.height} width={node.width} enableBackground="true" />
-                <text ref={nodeTextRef} y="50%" x="50%" dominantBaseline="middle" textAnchor="middle" pointerEvents="none"></text>
+                <text ref={nodeTextRef} y="50%" x="50%" dominantBaseline="middle" textAnchor="middle" pointerEvents="none">
+                    {node.type}
+                </text>
             </svg>
     );
 }
